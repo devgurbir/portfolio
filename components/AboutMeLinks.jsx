852ff4f220f45e3c5ff4046c5c3f1ae719901a12
devgurbir <91613146+devgurbir@users.noreply.github.com>
2022-04-03 14:53:48 +0530

    added links

--- components/AboutMeLinks.jsx
+++ components/AboutMeLinks.jsx
@@ -3,28 +3,28 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaArchive } from "react-icons/fa";
 import styles from "../styles/AboutMe.module.css";
-
+import Link from "next/link";
 const AboutMeLinks = () => {
   return (
     <div className={styles.aboutMeIcons}>
-      <a href="https://www.linkedin.com/in/gurbir-singh-20641a157/">
+      <Link href="https://www.linkedin.com/in/gurbir-singh-20641a157/">
         <div className={styles.aboutMeIconsDiv}>
           <FaLinkedin />
           <span>LinkedIn</span>
         </div>
-      </a>
-      <a href="https://github.com/devgurbir">
+      </Link>
+      <Link href="https://github.com/devgurbir">
         <div className={styles.aboutMeIconsDiv}>
           <FaGithub />
           <span>Github</span>
         </div>
-      </a>
-      <a href="#">
+      </Link>
+      <Link href="https://drive.google.com/file/d/1PL0rYXeRavLXAPO4YuiLK0lhh6G_ICU-/view">
         <div className={styles.aboutMeIconsDiv}>
           <FaArchive />
           <span>Resume</span>
         </div>
-      </a>
+      </Link>
     </div>
   );
 };
